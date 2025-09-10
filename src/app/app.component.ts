20c0abbd130593abfd120dd25671b1153a5aa102
Jeroen van Dragt <jeroen.van.dragt@outlook.com>
2025-09-10 17:40:58 +0200

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',  // 👈 use external HTML
+  styleUrls: ['./app.component.css']    // 👈 you can add styles herent.html',
 })
 export class AppComponent {
   value = 'World';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // needed if you use ngModel
 
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module'; // 👈 added routing
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule // 👈 routing comes after BrowserModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
